768856127f320b3051a9333cb08c15c64a2b4480
phuongnguyen1996 <46129227+phuongnguyen1996@users.noreply.github.com>
2019-03-13 02:44:01 +0700

    update signup, signin

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,33 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { DataService } from '../data.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from "@angular/forms";
+import { DataService } from "../data.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.css"],
 })
 export class SignInComponent implements OnInit {
   signInForm: FormGroup;
   error: string;
-  constructor(private dataService: DataService, private router: Router) {
-    this.signInForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required])
+  constructor(
+    private builder: FormBuilder,
+    private dataService: DataService,
+    private router: Router
+  ) {
+    this.signInForm = this.builder.group({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
     });
-   }
-
-  ngOnInit() {
   }
+
+  ngOnInit() {}
   onSubmit() {
-    this.dataService.postUserToLogin({user: this.signInForm.value}).subscribe((res) => {
-      this.error = undefined;
-      localStorage.setItem('myTodo', JSON.stringify(res));
-      this.router.navigateByUrl('/');
-    }, (error: any) => {
-      this.error = error.error.errors['email or password'][0];
-      console.log(this.error);
-    });
+    this.dataService.postUserToLogin({ user: this.signInForm.value }).subscribe(
+      res => {
+        this.error = undefined;
+        localStorage.setItem("myTodo", JSON.stringify(res));
+        this.router.navigateByUrl("/");
+      },
+      (error: any) => {
+        this.error = error.error.errors["email or password"][0];
+        console.log(this.error);
+      }
+    );
   }
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,41 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { DataService } from '../data.service';
-import { DataUser } from '../data.modle';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from "@angular/forms";
+import { DataService } from "../data.service";
+import { DataUser } from "../data.modle";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  selector: "app-sign-up",
+  templateUrl: "./sign-up.component.html",
+  styleUrls: ["./sign-up.component.css"],
 })
 export class SignUpComponent implements OnInit {
   signUpForm: FormGroup;
   errors: {
-    username: string[],
-    email: string[],
-    password: string[]
+    username: string[];
+    email: string[];
+    password: string[];
   };
-  constructor(private dataService: DataService, private router: Router) {
-    this.signUpForm = new FormGroup(
-      {
-        username: new FormControl('', [Validators.required]),
-        email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required])
+  constructor(
+    private builder: FormBuilder,
+    private dataService: DataService,
+    private router: Router
+  ) {
+    this.signUpForm = new FormGroup({
+      username: new FormControl("", [Validators.required]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  ngOnInit() {}
+  onSubmit() {
+    this.dataService.postUser({ user: this.signUpForm.value }).subscribe(
+      (res: DataUser) => {
+        this.errors = undefined;
+        this.dataService.dataUser = res;
+        localStorage.setItem("myTodo", JSON.stringify(res));
+        this.router.navigateByUrl("/");
+      },
+      (error: any) => {
+        this.errors = error.error.errors;
       }
     );
-   }
-
-  ngOnInit() {
-  }
-  onSubmit() {
-    this.dataService.postUser({user: this.signUpForm.value}).subscribe((res: DataUser) => {
-      this.errors = undefined;
-      this.dataService.dataUser = res;
-      localStorage.setItem('myTodo', JSON.stringify(res));
-      this.router.navigateByUrl('/');
-    }, (error: any) => {
-      this.errors = error.error.errors;
-    });
   }
 }
